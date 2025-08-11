7c9c4f291f0f09482a1d8e1e04f6b4f13ffd9c0e
Amit Kumar <tlbackend@jcsoftwaresolution.com>
2025-08-11 13:33:18 +0530

    key updates in prompt

--- app/models/prompts.js
+++ app/models/prompts.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const promptSchema = new mongoose.Schema({
+  //userId:{ type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   prompt: { type: String, required: true },
   response: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  isDeleted: { type: Boolean, default: false }
+  
+
 });
 
 module.exports = mongoose.model("Prompt", promptSchema);
